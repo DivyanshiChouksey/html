263b3b3fcdb56a55c5664d00e17ebbe0996287fb
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2021-10-23 17:12:35 +0530

    Create project.js

---
+++ project.js
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Divyanshi</title>
+  </head>
+  <body>
+    <form>
+        <div class="container " style="width: 20% ;margin-top: 20%;">
+        <input type="text" id="fname" name="fname" value="DivyanshiChouksey">
+        <button onclick="myFunction()">
+        Search
+        </button>
+        <script>
+            function myFunction() {
+              let str ="DivyanshiChuksey";
+                str.replace("https://github-readme-streak-stats.herokuapp.com/?user=DivyanshiChouksey");
+            }
+        </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+   
+  </body>
+</html>
